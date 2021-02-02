75957d44cecb100b05dc972505c88a7a2f0b29ab
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 13:52:38 +0000

    Using ass check to do testing

--- testing/test_main.py
+++ testing/test_main.py
@@ -1,9 +1,11 @@
+import AssCheck.funcchecks as fc
 import unittest
 from main import *
 
 class UnitTests(unittest.TestCase) :
     def test_function(self) :
+        inputs, outputs = np.zeros(20), np.zeros(20)
         for i in range(20) :
-            val = np.random.randint(1,9999)
-            nthou = int( np.floor( val / 1000) )
-            self.assertTrue( nthou==numberOfThousands(val), "Your function for determining the number of thousands is not working" )
+            inputs[i] = np.random.randint(1,9999)
+            outputs[i] = int( np.floor( val / 1000) )
+        assert( fc.check_func('numberOfThousands', inputs, outputs ) 
