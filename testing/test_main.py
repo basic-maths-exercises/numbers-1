26afa3a13e6d5d8138821bd352b66268a46b073d
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 14:25:58 +0000

    Fixed repl button and test

--- testing/test_main.py
+++ testing/test_main.py
@@ -4,8 +4,9 @@ from main import *
 
 class UnitTests(unittest.TestCase) :
     def test_function(self) :
-        inputs, outputs = np.zeros(20), np.zeros(20)
+        inputs, outputs = [], []
         for i in range(20) :
-            inputs[i] = np.random.randint(1,9999)
-            outputs[i] = int( np.floor( inputs[i] / 1000) )
+            val = np.random.randint(1,9999)
+            inputs.append((val,)) 
+            outputs.append(np.floor( val / 1000))
         assert( fc.check_func('numberOfThousands', inputs, outputs ) ) 
