9245a77c3609910897b4e0e6c173349002c75800
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-02 14:27:19 +0000

    Added answer

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def numberOfThousands( N ) :
   # Your code goes here
-  return 
+  return np.floor( N / 1000 )
 
 # This calls the function you have written to check if it works
 print( numberOfThousands(3260) )
